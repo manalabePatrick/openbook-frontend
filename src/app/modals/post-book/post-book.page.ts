e6fd2fa0f77a6fc216f162cedc19fdc0854bac92
manalabePatrick <78461378+manalabePatrick@users.noreply.github.com>
2021-04-12 08:54:45 +0800

    shit

--- src/app/modals/post-book/post-book.page.ts
+++ src/app/modals/post-book/post-book.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-post-book',
@@ -8,13 +9,49 @@ import { ModalController } from '@ionic/angular';
 })
 export class PostBookPage implements OnInit {
 
-  constructor(private modalController:ModalController) { }
+  public book = {
+    content: '',
+    theme: 'Body beybe'
+  }
+
+  constructor(private modalController:ModalController, private alertController: AlertController, private apiService: ApiService){ }
 
   ngOnInit() {
   }
 
   public addBook(){
-    this.modalController.dismiss();
+
+    // if(!this.book.title || !this.book.body) {
+    //   return this.presentAlert("All fields are required");
+    // }
+  
+  
+  let requestObject = {
+      location: "users/create-post",
+      method: "POST",
+      body: this.book
+  }
+  
+  this.apiService.makeRequest(requestObject).then((val) => {
+      if(val.statusCode == 201) {
+          val.newPost.ago = "Now";
+          //this.posts.col1.unshift(val.newPost);
+      } else {
+         // this.events.onAlertEvent.emit("Something went wrong, your post could not be created.");
+      }
+      //this.newPostContent = "";
+  });
+    //this.modalController.dismiss();
+  }
+
+  async presentAlert(msg) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class ApiService {
 
   baseURL = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private storage: LocalStorageService) { }
 
   public makeRequest(requestObject):any{
     const method = requestObject.method.toLowerCase();
@@ -16,7 +17,15 @@ export class ApiService {
     const location = requestObject.location;
 
     const url = `${this.baseURL}/${location}`;
-    let httpOptions = {};
+    let httpOptions = {}; 
+
+    if(this.storage.getToken()) {
+      httpOptions = {
+          headers: new HttpHeaders({
+              'Authorization': `Bearer ${this.storage.getToken()}`
+          })
+      }
+  }
 
     if(method ==="post"){
       return this.http.post(url, body, httpOptions).toPromise();
