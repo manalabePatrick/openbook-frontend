d16e7c2c03f358f396030035047665c8a21efaa6
manalabePatrick <78461378+manalabePatrick@users.noreply.github.com>
2021-04-15 17:54:32 +0800

    post function added

--- src/app/tabs/book-notes/book-notes.page.ts
+++ src/app/tabs/book-notes/book-notes.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-book-notes',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookNotesPage implements OnInit {
 
-  constructor() { }
+  bookId: any;
+  userId: any;
+  posts = [];
+  public post = {
+    content: '',
+    bookId: ''
+  }
+  constructor(private alertController: AlertController, private storage: LocalStorageService, private apiService:ApiService) { 
+    this.post.bookId = this.storage.bookId;
+    this.userId = this.storage.userId;
+    this.bookId = this.storage.bookId;
+  }
 
   ngOnInit() {
+    this.showPosts();
+  }
+
+  public showPosts(){
+    let requestObject = {
+      location: `users/get-user-data/${this.userId}`,
+      method: "GET"
+  }
+
+  this.apiService.makeRequest(requestObject).then((data) => {
+    this.posts = data.user.posts
+  });
+  }
+
+  public createPost(){
+    if(!this.post.content){
+      return this.presentAlert("You need to add something!")
+    }
+
+    let requestObject = {
+      location: "users/create-post",
+      method: "POST",
+      body: this.post
+    }
+
+    this.apiService.makeRequest(requestObject).then((val) => {
+      if(val.statusCode == 201) {
+          this.posts.push(val.newPost);
+          this.post.content = '';
+      } else {
+          console.log("Something went wrong, your post could not be created.");
+      }
+  });
+  }
+
+  async presentAlert(msg) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
